b48e13b2cf01c143d63887f3673fac3e5f6e5e5c
Preassume <aweraa29@gmail.com>
2021-09-02 00:30:56 -0500

    submit traffic lights WIP

---
+++ Assignments/P161/README.md
@@ -0,0 +1,12 @@
+# Assignment 4
+
+### Austin Riddle
+
+#### For CMPS 4883 Programming Techniques.
+
+#### 09/02/2021
+
+This is problem __161__ from https://onlinejudge.org/ called __Traffic Lights__.
+
+Unfortunately, my solution to this problem is still a work in progress.  There is something wrong with some of my output, and I have not yet figured out where I have gone wrong.
+
---
+++ Assignments/P161/traffic.cpp
@@ -0,0 +1,126 @@
+ /**
+ * Austin Riddle
+ * CMPS 4883
+ * 9/2/2021
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// A simple struct to contain necessary data for the light cycle
+// Also handles incrementation and checks for whether the light is green
+struct light{
+	int cycleTime;
+	int greenTime;
+	int curTime;
+	
+	// The constructor is supplied with the input from getScenario
+	light(int time){
+		cycleTime = time * 2; // The total time of the entire cycle
+		greenTime = time - 5; // The time spent being green
+		curTime = 0; // Our place in the cycle is initially zero
+	}
+	
+	// If our current position in the cycle is within the time spent
+	// being green, we return true. Otherwise, return false.
+	bool isGreen(){
+		return curTime <= greenTime;
+	}
+	
+	// Increment our position in the cycle. If we go over the cycle time,
+	// then it is time to reset our position to zero again.
+	void increment(){
+		curTime++;
+		if(curTime >= cycleTime) curTime = 0;
+	}
+};
+
+// Get a scenario from input and store it into our light vector
+// All traffic lights are green initially
+bool getScenario(vector<light*>& lights){
+	lights.clear();
+	int tmp;
+	cin >> tmp;
+	if(tmp == 0) return false; // Return false if we reach the end of the input
+	
+	while(tmp != 0){
+		lights.push_back(new light(tmp));
+		cin >> tmp;
+	}
+	return true;
+}
+
+// Fast forward our scenario until the first light(s) become yellow
+// At this instant, it's no longer true that all lights are green
+int initScenario(vector<light*>& lights){
+	int timer = 0;
+	bool go = true;
+	while(go){
+		timer++;
+		if(timer > 18000) break;
+		
+		for(auto x : lights){
+			x->increment();
+			if(!x->isGreen())
+				go = false; // Stop looping when the first light(s) become yellow
+		}
+	}
+	return timer;
+}
+
+// Now that not all lights are green, the timer can begin until we
+// encounter a state where all lights are green simultaniously again
+int simScenario(vector<light*>& lights){
+	int timer = 0;
+	bool go = true;
+	while(go){
+		timer++;
+		if(timer > 18000) break;
+		
+		go = false;
+		for(auto x : lights){
+			x->increment();
+			
+			if(x->isGreen() == false)
+				go = true; // Keep looping until all lights are green
+		}
+	}
+	return timer;
+}
+
+// Convert our result from seconds into hh:mm:ss format and print it
+void printTime(int totalSeconds){
+	if(totalSeconds > 18000){
+		cout << "Signals fail to synchronise in 5 hours\n";
+		return;
+	}
+	int seconds = totalSeconds;
+	
+	int minutes = totalSeconds / 60;
+	seconds -= minutes * 60;
+	
+	int hours = minutes / 60;
+	minutes -= hours * 60;
+	
+	cout << '0' << hours << ':';
+	
+	if(minutes < 10) cout << '0';
+	cout << minutes << ':';
+	
+	if(seconds < 10) cout << '0';
+	cout << seconds << '\n';
+}
+
+int main(){
+	vector<light*> lights; // Will hold one scenario at a time
+	
+	while(getScenario(lights)){ // Get one scenario from input
+		
+		//Initialize the scenario, then run the simulation. Save the time taken
+		int totalTime = initScenario(lights) + simScenario(lights);
+		
+		printTime(totalTime); // Print the result
+	}
+}
