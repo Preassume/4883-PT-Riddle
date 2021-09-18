641114456a3ca91a32000861df8f9496dbe5fac1
meat <meat@2603-8081-8207-3400-b21a-d8e3-bd17-5e5f.res6.spectrum.com>
2021-09-18 04:51:03 -0500

    Completed program and updated readme

--- Assignments/P11080/README.md
+++ Assignments/P11080/README.md
@@ -8,4 +8,7 @@
 
 This is problem __11080__ from https://onlinejudge.org/ called __Place the Guards__.
 
-My solution is still a work in progress.  As of right now, all but one of the test cases in ```data.dat``` gives the correct answer.  Stil trying to figure out that last one though!
+I was able to solve this problem by modifying some bipartite-detection code from https://www.geeksforgeeks.org/bipartite-graph/.  The primary modification was to the ```isBipartiteUtil``` function (which I shortened to ```isBipartite``` in my program.)  This function was modified to keep track of the changes made to the colors vector each time the function is called.  We can determine which color set contains the fewest elements for that particular function call and we return that value.  The values returned from this function are added together, and this sum is the smallest number of guards required to guard every junction.  If the data is found to not be a bipartite graph, then we return -1 and that is all that is printed.  This is because if the graph is not bipartite, then it is impossible to guard every junction without causing fights amongst the guards.
+
+
+Basically, my program grabs the input, creates an adjacency matrix out of it, and runs it through my modified geeksforgeeks functions.  This produces the minimum number of guards required to guard every junction without fighting.
--- Assignments/P11080/guard.cpp
+++ Assignments/P11080/guard.cpp
@@ -4,20 +4,32 @@
  * 9/14/2021
  */
 
-// This is still work in progress, so I haven't cleaned up and commented it yet. Sorry.
-
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <set>
+#include <map>
 
 using namespace std;
 
-int junctions, streets, disconJunctions;
+int junctions, streets;
 bool debug = false; // Set this to true for a more verbose output
 
-bool isBipartite(vector<vector<bool>>& adj, int src, vector<int>& colorVec){
+// Returns a -1 if a graph is not bipartite. Else, returns the number of elements
+// in the smallest set of colors possible.
+// This function is based on code found at https://www.geeksforgeeks.org/bipartite-graph/
+int isBipartite(vector<vector<bool>>& adj, int src, vector<int>& colorVec){
 	colorVec[src] = 1;
+	
+	// We must create a vector to keep track of the changes we made to colorVec
+	// during this function call. This is used to calculate the minimum number of guards.
+	vector<int> tmpColors;
+	tmpColors.resize(junctions, -1);
+	tmpColors[src] = 1; // We mimic every change done to colorVec
+	
+	// We use this map to keep track of the changes made to colorVec during this function call
+	map<bool, int> changeCount;
+	changeCount[1]++; // We changed an element in colorVec to 1, so we keep count of that
 	
 	queue<int> q;
 	q.push(src);
@@ -27,45 +39,52 @@ bool isBipartite(vector<vector<bool>>& adj, int src, vector<int>& colorVec){
 		q.pop();
 		
 		if(adj[u][u] == 1)
-			return false;
+			return -1;
 		
 		for(int v = 0; v < junctions; ++v){
 			if(adj[u][v] && colorVec[v] == -1){
 				colorVec[v] = 1 - colorVec[u];
+				changeCount[colorVec[v]]++; // Increment our counters in our map
 				q.push(v);
 			}
 			else if(adj[u][v] && colorVec[v] == colorVec[u])
-				return false;
+				return -1;
 		}
 	}
-	return true;
+	
+	// We want to return the number of elements in the smallest non-zero set of colors
+	if(changeCount[0] > 0 && changeCount[1] > 0)
+		return min(changeCount[0], changeCount[1]);
+		
+	else if(changeCount[0] > 0)
+		return changeCount[0];
+		
+	else if(changeCount[1] > 0)
+		return changeCount[1];
+		
+	return 0;
 }
 
+// This function will return the minimum number of guards required.
+// If it is not possible to guard every junction, it returns -1.
+// This function is based on code found at https://www.geeksforgeeks.org/bipartite-graph/
 int getNumGuards(vector<vector<bool>>& adj){
 	vector<int> colorVec;
 	colorVec.resize(junctions, -1);
 	
+	int guardCount = 0;
 	for(int i = 0; i < junctions; i++)
-		if(colorVec[i] == -1)
-			if(!isBipartite(adj, i, colorVec))
-				return -1;
-	
-	int counter = 0;
-	for(auto x : colorVec){
-		if(x == 1) counter++;
-		if(debug) cout << x << ' ';
-	}
-	if(debug) cout << "| ";
-	if(counter == 0) return 1;
-	
-	int dif = junctions - counter;
-	if(counter > dif) return dif;
-	return counter;
+		if(colorVec[i] == -1){
+			int tmp = isBipartite(adj, i, colorVec);
+			if(tmp == -1) return -1;
+			guardCount += tmp;
+		}
+	return guardCount;
 }
 
+// This creates the adjacency matrix based on the input
 vector<vector<bool>> getAdjMatrix(){
 	vector<vector<bool>> adj;
-	set<int> input;
 	
 	adj.resize(junctions);
 	for(int i = 0; i < junctions; i++){
@@ -75,12 +94,9 @@ vector<vector<bool>> getAdjMatrix(){
 	int f, t;
 	for(int i = 0; i < streets; i++){
 		cin >> f >> t;
-		input.insert(f);
-		input.insert(t);
 		adj[f][t] = true;
 		adj[t][f] = true;
 	}
-	disconJunctions = junctions - input.size();
 	return adj;
 }
 
@@ -91,34 +107,31 @@ int main(){
 	
 	int counter = 1;
 	while(tests){
-		
 		cin >> junctions >> streets;
+		auto adjMat = getAdjMatrix();
 		
+		if(debug){ // Verbose output if global variable 'debug' is true
+			cout << '\n' << counter << ":\n";
+			cout << junctions << ' ' << streets << '\n';
+			
+			// Print the adjacency matrix
+			for(auto x : adjMat){
+				for(auto y : x){
+					cout << y << ' ';
+				}
+				cout << '\n';
+			}
+			counter++;
+		}
+		
+		// If we have no streets, then we must have one guard for each junction
 		if(streets == 0){
-			if(debug) cout << counter << ":\n" << junctions << ' ' << streets << '\n';
 			cout << junctions << '\n';
-			if(debug) cout << '\n';
 		}
 		else{
-			auto adjMat = getAdjMatrix();
-			if(debug){
-				cout << counter << ":\n" << junctions << ' ' << streets << '\n';
-				for(auto x : adjMat){
-					for(auto y : x){
-						cout << y << ' ';
-					}
-					cout << '\n';
-				}
-			}
-			int guards = getNumGuards(adjMat);
-			if(guards == -1) cout << guards << '\n';
-			else cout << guards + disconJunctions << '\n';
-			if(debug){
-				cout << guards << ' ' << disconJunctions << '\n';
-			}
-			if(debug) cout << '\n';
+			cout << getNumGuards(adjMat) << '\n';
 		}
+		
 		tests--;
-		counter++;
 	}
 }
